ccfa2fa55e2987af7b41f57e37cf189c8b90aa73
sitihp <sitihamidah.pratama@gmail.com>
2014-10-09 14:31:26 +0700

    sip

---
+++ proses/kadis.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+$('#loading').show();
+
+get_data();
+
+function get_data(){
+	var AmbilData;
+	$.ajax({
+				type : 'GET',
+				url : 'http://sitihp.com/mobile/permohonankadis.php',
+				async: true,
+				beforeSend: function(x) {
+					if(x && x.overrideMimeType) {
+      					 x.overrideMimeType("application/j-son;charset=UTF-8");
+      				}
+				},
+				dataType : 'json',
+				success : function(data){
+					AmbilData = data.items;
+						
+						$('#loading').hide();
+						$('#tampilData').show();
+						$.each(AmbilData, function(index, loaddata) {
+						$('#sispakList').append(
+							'<li data-role="list-divider" data-theme="a" class="listview-custom"><a href="disposisikadis.html?id='+ loaddata.id_permohonan + '" data-ajax="false">'+ loaddata.id_permohonan + '</a></li>');
+						});
+					
+				},
+				error: function(jqXHR, exception) {
+					$('#loading').hide();
+					$('#conn_failed').show();
+				}
+		});	
+}	
+});
+
+
+
+
+
+
+
+
+
---
+++ proses/survei.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+$('#loading_panel').show();
+
+get_data();
+
+function get_data(){
+	var AmbilData;
+	$.ajax({
+				type : 'GET',
+				url : 'http://sitihp.com/mobile/permohonansurvei.php',
+				async: true,
+				beforeSend: function(x) {
+					if(x && x.overrideMimeType) {
+      					 x.overrideMimeType("application/j-son;charset=UTF-8");
+      				}
+				},
+				dataType : 'json',
+				success : function(data){
+					AmbilData = data.items;
+					if(AmbilData=='')
+					{
+						$('#loading_panel').hide();
+						$('#empty').show();
+					}
+					else
+					{						
+						$('#loading_panel').hide();
+						$('#tampilData').show();
+						$.each(AmbilData, function(index, loaddata) {
+						$('#sispakList').append(
+							'<table><tr></tr></table>',
+							'<li data-role="list-divider" data-theme="a" class="listview-custom"><a href="surveiinsert.html?id='+ loaddata.id_permohonan + '" data-ajax="false">'+ loaddata.id_permohonan + '</a></li>');
+						});
+					}
+				},
+				error: function(jqXHR, exception) {
+					$('#loading').hide();
+					$('#conn_failed').show();
+				}
+		});	
+}	
+});
+
+
+
+
+
+
+
+
+
